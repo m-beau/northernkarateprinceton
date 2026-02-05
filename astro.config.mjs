27b3a57962d9a7a1985fb40a536140487fbe4319
m-beau <maximebeaujeanroch047@gmail.com>
2026-02-05 11:37:18 -0500

    added sitemap for google indexing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://northernkarateprinceton.com',
-  output: 'static'
+  output: 'static',
+  integrations: [sitemap()]
 });
